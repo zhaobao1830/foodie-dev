87c6d1c5259ddbbe467781cf2eb7898bee4abd50
zhaobao1830 <x5219438>
2020-01-14 11:34:37 +0800

    1

--- foodie-dev-mapper/src/main/java/com/zb/dao/StuMapper.java
+++ foodie-dev-mapper/src/main/java/com/zb/dao/StuMapper.java
@@ -1,8 +1,9 @@
 package com.zb.dao;
 
+import com.zb.dao.mapper.MyMapper;
 import com.zb.pojo.Stu;
 
-public interface StuMapper {
+public interface StuMapper extends MyMapper {
     int deleteByPrimaryKey(Integer id);
 
     int insert(Stu record);
---
+++ foodie-dev-mapper/src/main/java/com/zb/dao/mapper/MyMapper.java
@@ -0,0 +1,8 @@
+package com.zb.dao.mapper;
+
+import com.zb.pojo.Stu;
+
+public interface MyMapper {
+
+    Stu selectByPrimaryKey(Integer id);
+}
