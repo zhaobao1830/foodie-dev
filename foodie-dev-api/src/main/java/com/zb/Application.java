bd8864d29c1e1d0a3f0d53cd4d37da1fa724ac13
zhaobao1830 <x5219438>
2020-01-14 09:56:10 +0800

    1

--- foodie-dev-api/src/main/java/com/zb/Application.java
+++ foodie-dev-api/src/main/java/com/zb/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
-@MapperScan(basePackages = "com.zb.mapper")
+@MapperScan(basePackages = "com.zb.dao")
 public class Application {
 
     public static void main(String[] args) {
