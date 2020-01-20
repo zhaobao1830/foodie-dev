157037062ec15adb32ee4c429d2a5d201329ae89
zhaobao1830 <x5219438>
2020-01-20 10:15:53 +0800

    UserBo文件  从前端传递过来的属性都放在这个文件

---
+++ foodie-dev-pojo/src/main/java/com/zb/pojo/bo/UserBO.java
@@ -0,0 +1,33 @@
+package com.zb.pojo.bo;
+
+public class UserBO {
+    // 添加从前端传递过来的属性
+
+    private String username;
+    private String password;
+    private String confirmPassword;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
