745adf6c730e62b44cd97788a320c16fea2aed16
zhaobao1830 <x5219438>
2020-02-11 17:58:39 +0800

    spring-boot解决跨域

---
+++ foodie-dev-api/src/main/java/com/zb/config/CorsConfig.java
@@ -0,0 +1,37 @@
+package com.zb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    public CorsConfig() {
+
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        // 1、添加cors配置信息
+        CorsConfiguration config = new CorsConfiguration();
+
+        // http://localhost:8080 为前端发起接口的地址
+        config.addAllowedOrigin("http://localhost:8080");
+
+        // 设置是否发送cookie信息
+        config.setAllowCredentials(true);
+
+        config.addAllowedMethod("*");
+
+        config.addAllowedHeader("*");
+
+        // 2、为url添加映射路径
+        UrlBasedCorsConfigurationSource corsSource = new UrlBasedCorsConfigurationSource();
+        corsSource.registerCorsConfiguration("/**", config);
+
+        // 3、返回重新设定好的corsSource
+        return new CorsFilter(corsSource);
+    }
+}
