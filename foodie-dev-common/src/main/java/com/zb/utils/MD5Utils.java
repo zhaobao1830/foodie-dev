81637f1c80c7022ea5ffc59d73b93439ef4efaac
zhaobao1830 <x5219438>
2020-02-12 12:00:09 +0800

    修改md5加密方法，添加获取配置文件的方法

--- foodie-dev-common/src/main/java/com/zb/utils/MD5Utils.java
+++ foodie-dev-common/src/main/java/com/zb/utils/MD5Utils.java
@@ -6,23 +6,42 @@ import java.security.MessageDigest;
 
 public class MD5Utils {
 
-	/**
-	 * 
-	 * @Title: MD5Utils.java
-	 * @Description: 对字符串进行md5加密
-	 */
-	public static String getMD5Str(String strValue) throws Exception {
-		MessageDigest md5 = MessageDigest.getInstance("MD5");
-		String newstr = Base64.encodeBase64String(md5.digest(strValue.getBytes()));
-		return newstr;
+	private static String byteArrayToHexString(byte b[]) {
+		StringBuffer resultSb = new StringBuffer();
+		for (int i = 0; i < b.length; i++)
+			resultSb.append(byteToHexString(b[i]));
+
+		return resultSb.toString();
 	}
 
-	public static void main(String[] args) {
-		try {
-			String md5 = getMD5Str("imooc");
-			System.out.println(md5);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	private static String byteToHexString(byte b) {
+		int n = b;
+		if (n < 0)
+			n += 256;
+		int d1 = n / 16;
+		int d2 = n % 16;
+		return hexDigits[d1] + hexDigits[d2];
 	}
+
+	private static String MD5Encode(String origin, String charsetname) {
+		String resultString = null;
+		try {
+			resultString = new String(origin);
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			if (charsetname == null || "".equals(charsetname))
+				resultString = byteArrayToHexString(md.digest(resultString.getBytes()));
+			else
+				resultString = byteArrayToHexString(md.digest(resultString.getBytes(charsetname)));
+		} catch (Exception exception) {
+		}
+		return resultString.toUpperCase();
+	}
+
+	public static String MD5EncodeUtf8(String origin) {
+		origin = origin + PropertiesUtil.getProperty("password.salt", "");
+		return MD5Encode(origin, "utf-8");
+	}
+
+	private static final String hexDigits[] = {"0", "1", "2", "3", "4", "5",
+			"6", "7", "8", "9", "a", "b", "c", "d", "e", "f"};
 }
