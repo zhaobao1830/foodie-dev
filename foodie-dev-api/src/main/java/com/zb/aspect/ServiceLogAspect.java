f5cf39ac80f836de9e8ba9c5e35b4604a7df68f0
zhaobao1830 <373098777@qq.com>
2020-07-24 09:57:37 +0800

    通过日志监控service执行时间完成

--- foodie-dev-api/src/main/java/com/zb/aspect/ServiceLogAspect.java
+++ foodie-dev-api/src/main/java/com/zb/aspect/ServiceLogAspect.java
@@ -2,10 +2,12 @@ package com.zb.aspect;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+@Aspect
 @Component
 public class ServiceLogAspect {
     public static final Logger log = LoggerFactory.getLogger(ServiceLogAspect.class);
