ab4d304cdb5254177ba5450dcb60c0efe387d67b
zhaobao1830 <373098777@qq.com>
2024-02-06 22:39:18 +0800

    修改CarouselMapper

--- foodie-dev-mapper/src/main/java/com/zb/mapper/CategoryMapper.java
+++ foodie-dev-mapper/src/main/java/com/zb/mapper/CategoryMapper.java
@@ -1,5 +1,6 @@
 package com.zb.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.zb.pojo.model.Category;
 import com.zb.pojo.vo.CategoryVO;
 import com.zb.pojo.vo.NewItemsVO;
@@ -8,7 +9,7 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 import java.util.Map;
 
-public interface CategoryMapper {
+public interface CategoryMapper extends BaseMapper<Category> {
     int deleteByPrimaryKey(Integer id);
 
     int insert(Category record);
@@ -21,7 +22,7 @@ public interface CategoryMapper {
 
     int updateByPrimaryKey(Category record);
 
-    List<Category> selectAllCategory(Integer type);
+//    List<Category> selectAllCategory(Integer type);
 
     public List<CategoryVO> getSubCatList(Integer rootCatId);
 
