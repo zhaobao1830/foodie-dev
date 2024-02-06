06be1de23af7a1b44fc360a784c69576c743d51b
zhaobao1830 <373098777@qq.com>
2024-02-06 22:38:43 +0800

    修改CarouselMapper

--- foodie-dev-mapper/src/main/java/com/zb/mapper/CarouselMapper.java
+++ foodie-dev-mapper/src/main/java/com/zb/mapper/CarouselMapper.java
@@ -6,17 +6,4 @@ import com.zb.pojo.model.Carousel;
 import java.util.List;
 
 public interface CarouselMapper extends BaseMapper<Carousel> {
-    int deleteByPrimaryKey(String id);
-
-    int insert(Carousel record);
-
-    int insertSelective(Carousel record);
-
-    Carousel selectByPrimaryKey(String id);
-
-    int updateByPrimaryKeySelective(Carousel record);
-
-    int updateByPrimaryKey(Carousel record);
-
-    List<Carousel> selectAllCarousel(Integer isShow);
 }
