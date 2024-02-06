7a25b8983c8f889fa29ddea4a42144e1bbe00e9d
zhaobao1830 <373098777@qq.com>
2024-02-06 22:39:44 +0800

    修改CarouselMapper

--- foodie-dev-mapper/src/main/java/com/zb/mapper/CarouselMapper.java
+++ foodie-dev-mapper/src/main/java/com/zb/mapper/CarouselMapper.java
@@ -3,7 +3,5 @@ package com.zb.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.zb.pojo.model.Carousel;
 
-import java.util.List;
-
 public interface CarouselMapper extends BaseMapper<Carousel> {
 }
