89e2c1610addc7ba4c2c9c3b308510e9473eb080
zhaobao1830 <373098777@qq.com>
2024-02-07 08:37:54 +0800

    删除无用的@return注释

--- foodie-dev-service/src/main/java/com/zb/service/CategoryService.java
+++ foodie-dev-service/src/main/java/com/zb/service/CategoryService.java
@@ -10,21 +10,18 @@ public interface CategoryService {
 
     /**
      * 查询所有一级分类
-     * @return
      */
     List<Category> queryAllRootLevelCat();
 
     /**
      * 根据一级分类id查询子分类信息
      * @param rootCatId 分类id
-     * @return
      */
     List<CategoryVO> getSubCatList(Integer rootCatId);
 
     /**
      * 查询首页每个一级分类下的6条最新商品数据
      * @param rootCatId 分类id
-     * @return
      */
     List<NewItemsVO> getSixNewItemsLazy(Integer rootCatId);
 }
