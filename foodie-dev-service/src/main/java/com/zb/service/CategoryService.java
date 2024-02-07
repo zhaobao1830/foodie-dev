2c9660f64e35af7fb33e4b6d5528918f44ef3fac
zhaobao1830 <373098777@qq.com>
2024-02-07 08:37:09 +0800

    删除无用的public

--- foodie-dev-service/src/main/java/com/zb/service/CategoryService.java
+++ foodie-dev-service/src/main/java/com/zb/service/CategoryService.java
@@ -12,19 +12,19 @@ public interface CategoryService {
      * 查询所有一级分类
      * @return
      */
-    public List<Category> queryAllRootLevelCat();
+    List<Category> queryAllRootLevelCat();
 
     /**
      * 根据一级分类id查询子分类信息
      * @param rootCatId 分类id
      * @return
      */
-    public List<CategoryVO> getSubCatList(Integer rootCatId);
+    List<CategoryVO> getSubCatList(Integer rootCatId);
 
     /**
      * 查询首页每个一级分类下的6条最新商品数据
      * @param rootCatId 分类id
      * @return
      */
-    public List<NewItemsVO> getSixNewItemsLazy(Integer rootCatId);
+    List<NewItemsVO> getSixNewItemsLazy(Integer rootCatId);
 }
