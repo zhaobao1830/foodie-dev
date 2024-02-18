8189d653b280ec7af14564d825b09c83d6abe262
zhaobao1830 <373098777@qq.com>
2024-02-18 15:21:10 +0800

    用户下单方法：RestTemplate注入有问题，先将其注销

--- foodie-dev-api/src/main/java/com/zb/controller/OrdersController.java
+++ foodie-dev-api/src/main/java/com/zb/controller/OrdersController.java
@@ -37,8 +37,8 @@ public class OrdersController extends BaseController{
     @Autowired
     private OrderService orderService;
 
-    @Autowired
-    private RestTemplate restTemplate;
+//    @Autowired
+//    private RestTemplate restTemplate;
 
     @ApiOperation(value = "用户下单", notes = "用户下单", httpMethod = "POST")
     @PostMapping("/create")
@@ -71,15 +71,19 @@ public class OrdersController extends BaseController{
         HttpEntity<MerchantOrdersVO> entity =
                 new HttpEntity<>(merchantOrdersVO, headers);
 
-        ResponseEntity<IMOOCJSONResult> responseEntity =
-                restTemplate.postForEntity(paymentUrl,
-                        entity,
-                        IMOOCJSONResult.class);
-        IMOOCJSONResult paymentResult = responseEntity.getBody();
-        if (paymentResult != null && paymentResult.getStatus() != 200) {
-            logger.error("发送错误：{}", paymentResult.getMsg());
-            return IMOOCJSONResult.errorMsg("支付中心订单创建失败，请联系管理员！");
-        }
+//        ResponseEntity<IMOOCJSONResult> responseEntity =
+//                restTemplate.postForEntity(paymentUrl,
+//                        entity,
+//                        IMOOCJSONResult.class);
+//        ResponseEntity<IMOOCJSONResult> responseEntity =
+//                restTemplate.postForEntity(paymentUrl,
+//                        entity,
+//                        IMOOCJSONResult.class);
+//        IMOOCJSONResult paymentResult = responseEntity.getBody();
+//        if (paymentResult != null && paymentResult.getStatus() != 200) {
+//            logger.error("发送错误：{}", paymentResult.getMsg());
+//            return IMOOCJSONResult.errorMsg("支付中心订单创建失败，请联系管理员！");
+//        }
 
         return IMOOCJSONResult.ok(orderId);
     }
